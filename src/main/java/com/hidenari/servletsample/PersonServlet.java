0316b19b0b3744e4ca075567733d41347dc37712
hide-nari <hide3@mild.ocn.ne.jp>
2026-05-30 05:43:18 +0900

    Add person.jsp and init param logic

--- src/main/java/com/hidenari/servletsample/PersonServlet.java
+++ src/main/java/com/hidenari/servletsample/PersonServlet.java
@@ -1,22 +1,17 @@
 package com.hidenari.servletsample;
 
-import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class PersonServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + getServletConfig().getInitParameter("initMsg") + "</h1>");
-        out.println("<h1>" + getServletContext().getInitParameter("conParam") + "</h1>");
-        out.println("</body></html>");
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.setAttribute("initMsg", getServletConfig().getInitParameter("initMsg"));
+        request.setAttribute("conParam", getServletContext().getInitParameter("conParam"));
+        request.getRequestDispatcher("person.jsp").forward(request, response);
     }
 
 }
