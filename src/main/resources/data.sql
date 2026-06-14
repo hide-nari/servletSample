3e4e33f0b24ff37cc3f9b73701f826f5dcf577ed
hide-nari <hide3@mild.ocn.ne.jp>
2026-06-14 09:10:19 +0900

    Add database auth

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `users` (`id`, `uid`, `password`)
+VALUES (1, 'taro',
+        '286d33a69400a238286ab5d3d18746c6db168de70c92a98983a764b3333f0fe4$1$6417f6a090605518381526ae02cdfe7a88c0dc8d11e8c11146ec253901a30d6cbc3f99bc04727d27b178ba3f53572c007fb227c556aa798f7c23b2316673bf38');
+
+INSERT INTO `roles` (`id`, `uid`, `role`)
+VALUES (1, 'taro', 'admin');
