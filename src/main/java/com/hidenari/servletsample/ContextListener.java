17dc5c4b8590ebffa7e7850a942013a37bd221ae
hide-nari <hide3@mild.ocn.ne.jp>
2026-06-14 15:03:32 +0900

    Mod context listener

--- src/main/java/com/hidenari/servletsample/ContextListener.java
+++ src/main/java/com/hidenari/servletsample/ContextListener.java
@@ -5,7 +5,7 @@ import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
-@WebListener
+//@WebListener
 public class ContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
